72d68d01510cb7f68bb2ee4ab8cb5982939a89c6
ftao <fft1026@163.com>
2018-06-25 19:46:17 +0800

    # 2.4.0 多码率功能 - 项目结构调整 - 新增多码率功能 - 部分UI更新：   - 播放器默认更新为 16:9   - 新增 icon、添加加载视图等

---
+++ PolyvLiveSDKDemo/PolyvLiveSDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PolyvLiveSDKDemo
+//
+//  Created by ftao on 24/05/2018.
+//  Copyright © 2018 easefun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
